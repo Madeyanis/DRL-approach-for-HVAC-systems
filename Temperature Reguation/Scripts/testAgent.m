9dc2d2f527f0f71e68f92337a46717b5a9b355e2
Yanis Masdoua <masdoua.yanis@gmail.com>
2022-07-12 13:20:49 +0200

    updates

--- Temperature Reguation/Scripts/testAgent.m
+++ Temperature Reguation/Scripts/testAgent.m
@@ -30,11 +30,11 @@ Atz = -0.15;
 Bh = mdot/(3*M);
 
 
-% Tout = [7 6 5 4 4 5 6 9 5 7 6 7 8 8 7 8 8 7 8 9 8 9 8 8 7 7 6 6 6 5];
+Tout = [7 6 5 4 4 5 6 9 5 7 6 7 8 8 7 8 8 7 8 9 8 9 8 8 7 7 6 6 6 5];
 % aaa = 5;
 % bbb = 18;
 % Tout = (bbb-aaa).*rand(Tf/Ts,1) + aaa;
-Tout = 6*ones(1, 24);
+% Tout = 6*ones(1, 24);
 % Ref = [zeros(1, 7) 20*ones(1, 9) zeros(1, 8)];
 Ref = 20*ones(1, 24);
 % Ref = [zeros(1, Tf/3) 19*ones(1,2*(Tf/3))];
